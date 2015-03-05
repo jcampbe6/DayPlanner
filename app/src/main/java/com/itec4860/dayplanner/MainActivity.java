5dc90c1079cc77f248ff05b4898e3810dd72e120
jcampbe6 <jcampbe6@ggc.edu>
2015-03-05 00:26:10 -0500

    created blank android project

---
+++ app/src/main/java/com/itec4860/dayplanner/MainActivity.java
@@ -0,0 +1,35 @@
+package com.itec4860.dayplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**Class: MainActivity
+ * @author Joshua Campbell
+ * @version 1.0
+ * Course: ITEC 4860 Spring 2015
+ * Written: March 5, 2015
+ *
+ * This class will check if a user registration has occurred on this application.
+ * If a user registration has not occurred, then it will switch to a registration screen.
+ * If a user registration has occurred, then it will switch to the default calendar screen.
+ *
+ * Purpose: This class is the initial class that is started when the Day Planner
+ * mobile device application is first initiated by a user of the application.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //check for registration status
+        //then switch to the appropriate screen/activity
+
+        //NOTE: it currently sets the view to the default 'activity_main' with a 'Hello World'
+        //text view - I will change this
+
+        setContentView(R.layout.activity_main);
+    }
+
+}
