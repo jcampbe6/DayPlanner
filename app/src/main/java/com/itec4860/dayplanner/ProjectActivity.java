f7ae893e76cd5a622741e4c6f1d594671493bb4a
jcampbe6 <jcampbe6@ggc.edu>
2015-04-09 01:15:25 -0400

    Revert "Basic Event and Project Activities"

--- app/src/main/java/com/itec4860/dayplanner/ProjectActivity.java
+++ app/src/main/java/com/itec4860/dayplanner/ProjectActivity.java
@@ -1,71 +1,8 @@
 package com.itec4860.dayplanner;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Toast;
-
 /**
  * Created by Timothy on 4/8/2015.
  */
-public class ProjectActivity extends Activity{
-
-    private Spinner projectTypeSpinner;
-    private Button saveButton;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.project_view);
-
-        addItemsOnSpinner();
-        addListenerOnButton();
-        addListenerOnSpinnerItemSelection();
-    }
-
-    // add items into spinner dynamically
-    public void addItemsOnSpinner() {
-
-        projectTypeSpinner = (Spinner) findViewById(R.id.projectTypeSpinner);
-        List<String> list = new ArrayList<String>();
-        list.add("Month");
-        list.add("Week");
-        list.add("Day");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        projectTypeSpinner.setAdapter(dataAdapter);
-    }
-
-    public void addListenerOnSpinnerItemSelection() {
-        projectTypeSpinner = (Spinner) findViewById(R.id. projectTypeSpinner);
-        projectTypeSpinner.setOnItemSelectedListener(new CustomProjectTypeListener());
-    }
-
-    // get the selected dropdown list value
-    public void addListenerOnButton() {
-
-        projectTypeSpinner = (Spinner) findViewById(R.id.projectTypeSpinner);
-
-//the save button doesn't do anything important right now
-        saveButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(ProjectActivity.this,
-                        "OnClickListener : " + "\nProject Type: " +
-                                String.valueOf(projectTypeSpinner.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
-            }
-
-        });
-    }
+public class ProjectActivity {
+    //The true test
 }
