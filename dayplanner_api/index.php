35e91bc7995c7b005a7b8a3893bd9014746755be
jcampbe6 <jcampbe6@ggc.edu>
2015-04-22 11:44:36 -0400

    working on 'save event' funtionality in API

--- dayplanner_api/index.php
+++ dayplanner_api/index.php
@@ -14,24 +14,23 @@ if (isset($_POST["tag"]) && $_POST["tag"] != "")
 {
 	// get tag
 	$tag = $_POST["tag"];
-
-	// include database handler
-    include_once 'DayPlannerDatabaseHandler.php';
-	$databaseHandler = new DayPlannerDatabaseHandler();
 	
 	// response Array in JSON format
 	$jsonResponse = array("error" => false);
 
     // check "tag" for request type
-    // so far the possible request types are: 'register' and 'login'
+    // so far the possible request types are: 'register', 'login' and 'save event'
     if ($tag == "register")
     {
+        include_once 'UserRegLoginHandler.php';
+        $userRegHandler = new UserRegLoginHandler();
+
         $regUsername = $_POST["username"];
 		$regEmail = $_POST["email"];
 		$regPassword = $_POST["password"];
 
 		// check if user already exists
-        $errorMsg = $databaseHandler->doesUserExist($regUsername, $regEmail);
+        $errorMsg = $userRegHandler->doesUserExist($regUsername, $regEmail);
 
 		if ($errorMsg) // user already exists --> respond with error message
         {
@@ -43,7 +42,7 @@ if (isset($_POST["tag"]) && $_POST["tag"] != "")
 
 		else // there is no error message --> user doesn't exist --> store user
         {
-			$registeredUser = $databaseHandler->storeUser($regUsername, $regEmail, $regPassword);
+			$registeredUser = $userRegHandler->storeUser($regUsername, $regEmail, $regPassword);
 
 			if ($registeredUser) // user stored successfully
             {
@@ -65,12 +64,15 @@ if (isset($_POST["tag"]) && $_POST["tag"] != "")
 
     else if ($tag == "login")
     {
+        include_once 'UserRegLoginHandler.php';
+        $userLoginHandler = new UserRegLoginHandler();
+
         // Request type is check Login
         $loginUsernameOrEmail = $_POST['usernameOrEmail'];
         $loginPassword = $_POST['password'];
 
         // check for user
-        $authenticatedUser = $databaseHandler->authenticateUser($loginUsernameOrEmail, $loginPassword);
+        $authenticatedUser = $userLoginHandler->authenticateUser($loginUsernameOrEmail, $loginPassword);
 
         if ($authenticatedUser) // user email and password match
         {
@@ -89,7 +91,51 @@ if (isset($_POST["tag"]) && $_POST["tag"] != "")
         }
     }
 
-	else // received a tag other than 'register' or 'login'
+    else if ($tag == "save event")
+    {
+        include_once 'EventHandler.php';
+        $saveEventHandler = new EventHandler();
+
+        // general event info
+        $eventTitle = $_POST['eventTitle'];
+        $eventType = $_POST['eventType'];
+
+        $eventType = $_POST['eventType'];
+        if ($eventType == "Project")
+        {
+            // project event-type info
+            $startDate = $_POST['startDate'];
+            $endDate = $_POST['endDate'];
+
+            $tasks = array("hasTask" => false);
+            if (isset($_POST['taskName']) && $_POST['taskName'][0] != "")
+            {
+                $tasks["hasTask"] = true;
+
+                for ($i = 0; $i < count($_POST['taskName']); $i++)
+                {
+                    $taskNum = $i + 1;
+                    if ($_POST['taskName'][$i] != "")
+                    {
+                        $tasks["task$taskNum"] = array("taskName" => $_POST['taskName'][$i], "dueDate" => $_POST['taskDueDate'][$i],
+                            "completed" => $_POST['completedStatus'][$i]);
+                    }
+                }
+            }
+
+            if ($saveEventHandler->saveNewProjectEvent())
+            {
+                echo json_encode($tasks). "<br><br>";
+            }
+        }
+
+        $userID = $_POST['userID'];
+
+
+        echo json_encode($_POST);
+    }
+
+	else // received a tag other than 'register', 'login' or 'save event'
     {
         $jsonResponse["error"] = true;
         $jsonResponse["errorMsg"] = "Invalid request.";
